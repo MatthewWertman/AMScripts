7eb8688d405edf85af0bea7c4d7a5fdaed9f5860
MatthewWertman <matthewwertman57@gmail.com>
2021-12-09 20:50:52 -0800

    rewrite

--- extension/popup.js
+++ extension/popup.js
@@ -1,36 +1,38 @@
+const tabQuery = {
+    active: true,
+    currentWindow: true,
+    url: ["https://cloud.awesomeminer.com/miners", "http://192.168.101.100:17790/miners"]
+}
+const expandGroups = document.getElementById("expandGroupsBtn");
+const collapseGroups = document.getElementById("collapseGroupsBtn");
 
-let expandGroups = document.getElementById("expandGroupsBtn");
-
+// Listeners
 expandGroups.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true, url: ["https://cloud.awesomeminer.com/miners", "http://192.168.101.100:17790/miners"] });
+    let [tab] = await chrome.tabs.query(tabQuery);
 
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        function: execExpandGroupsUtil,
+        function: exec,
+        args: ["datatable-row-wrapper", "fa-chevron-right", "bitcap-expandgroups", "Expanding"]
     });
 });
 
-// The body of this function will be executed as a content script inside the
-// current page
-function execExpandGroupsUtil() {
-    // // get all group header rows
-    let rows = Array.from(document.getElementsByClassName('datatable-row-wrapper'));
-    // loop through each header row and determine where the next one will be
-    // after being expanded
-    const rowOffsets = [];
-    for (let row in rows) {
-        const headerText = rows[row].querySelectorAll('span')[2].textContent.trim();
-        rowOffsets.push(Number(headerText.slice(headerText.indexOf("Total") + 7, headerText.length - 1)));
+collapseGroups.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query(tabQuery);
+
+    chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        function: exec,
+        args: ["datatable-row-wrapper", "fa-chevron-down", "bitcap-collapsegroups", "Collapsing"]
+    });
+});
+
+function exec(class1, class2, utilName, actionString) {
+    const rowCount = document.getElementsByClassName(class1).length;
+    let rows = document.getElementsByClassName(class2);
+    console.log(`[${utilName}] ${actionString} groups...`);
+    for (let i = 0; i < rowCount; i++) {
+        rows[0].click();
     }
-    console.log('[bitcap-expandgroups] Expanding groups...');
-    let rowPointer = 0;  // keep track of what row we are on
-    for (let i = 0; i < rowOffsets.length; i++) {
-        // trigger click event on row of rowPointer
-        rows[rowPointer].querySelector('i.fa-chevron-right').click();
-        // refresh the total number of rows
-        rows = Array.from(document.getElementsByClassName('datatable-row-wrapper'));
-        // set our counter to the position of the next group header row
-        rowPointer += rowOffsets[i] + 1;
-    }
-    console.log('[bitcap-expandgroups] Done!');
+    console.log(`[${utilName}] Done!`);
 }
