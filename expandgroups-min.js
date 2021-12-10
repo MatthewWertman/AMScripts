3cfd133d5715a3b1bd4771a234683e4a7db2797e
MatthewWertman <matthewwertman57@gmail.com>
2021-12-09 23:53:31 -0800

    reminify snippet

--- expandgroups-min.js
+++ expandgroups-min.js
@@ -1,17 +1,1 @@
-var expandGroups;
-(expandGroups = function () {
-    let rA = Array.from(document.getElementsByClassName('datatable-row-wrapper'));
-    const rOffs = [];
-    for (let r in rA) {
-    	const t = rA[r].querySelectorAll('span')[2].textContent.trim();
-        rOffs.push(Number(t.slice(t.indexOf("Total") + 7, t.length - 1)));
-    };
-    console.log('[bitcap-expandgroups] Expanding groups...');
-    let rP = 0;
-    for (let o = 0; o < rOffs.length; o++) {
-        rA[rP].querySelector('i.fa-chevron-right').click();
-        rA = Array.from(document.getElementsByClassName('datatable-row-wrapper'));
-        rP += rOffs[o] + 1;
-    }
-    console.log('[bitcap-expandgroups] Done!');
-})();
+var expandGroups;(expandGroups=function(){let r=Array.from(document.getElementsByClassName("datatable-row-wrapper"));const t=[];for(var e in r){const a=r[e].querySelectorAll("span")[2].textContent.trim();t.push(Number(a.slice(a.indexOf("Total")+7,a.length-1)))}console.log("[bitcap-expandgroups] Expanding groups...");let o=0;for(let e=0;e<t.length;e++)r[o].querySelector("i.fa-chevron-right").click(),r=Array.from(document.getElementsByClassName("datatable-row-wrapper")),o+=t[e]+1;console.log("[bitcap-expandgroups] Done!")})()
