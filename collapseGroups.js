fe9715bff85c14d823102a2ac8d16832c6a28a67
MatthewWertman <matthewwertman57@gmail.com>
2021-12-10 21:18:12 -0800

    create collapseGroups.js snippet

---
+++ collapseGroups.js
@@ -0,0 +1,23 @@
+ // A simple and "hacky" function that COLLAPSES all group header rows in AM Cloud.
+// withj the added measure for not locking the website.
+// Author: MatthewWertman
+// Date created: 12/10/21
+let rows = document.getElementsByClassName("fa-chevron-down");
+const length = rows.length;
+var index = 0;
+console.log('[bitcap-collapsegroups] Collapsing groups...');
+var worker = function() {
+  for (; index < length; index++) {
+    rows[0].click();
+    // here we delay the worker a little bit every so often to prevent
+    // the website from locking up.
+    if (index + 1 < length && index % 100 == 0) {
+        setTimeout(worker, 5);
+        break;
+    }
+  }
+};
+console.time();
+worker();
+console.timeEnd();
+console.log('[bitcap-collapsegroups] Done!');
